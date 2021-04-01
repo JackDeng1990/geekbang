c98ebf23f54543b695597780254b94b363310c37
JackDeng1990 <dc15922121696@gmail.com>
2021-04-01 18:33:46 +0800

    add: 补交选做1，修改必做3

---
+++ jvm/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * @author dengchao
+ */
+public class Hello {
+
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        if (true) {
+            c++;
+        }
+        for ( int i = 0; i < 4; i++) {
+            c+=i;
+        }
+    }
+}
